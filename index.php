02b8c08a8990626763298ac184c70767538222a0
raneemkhalid45 <137226775+raneemkhalid45@users.noreply.github.com>
2023-06-20 14:17:13 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="E:\proj\stylesheet\login1.css">
+<title>Imtiaz Store Sales Management System</title>
+<style>
+.hero-image {
+  
+  background-color: #3c3c3c;
+  height: 500px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: White;
+}
+</style>
+<body>
+<center>
+	<h1><b><u>Imtiaz Store Sales Management System</h1>
+</center>	
+<div class="hero-image">
+  <div class="hero-text">
+
+    <h1 style="font-size:60px;color:blue"><b>Welcome</h1>
+    <h2 style="color:green;font-size:40px"><u><b>Login </h2>
+    <a href="admin/dlogin.php"> <button style="font-size:25px ;background-color:#000080">Admin Login</button></a>
+    <a href="slogin.php"> <button style="font-size:25px ;background-color:#800000">Store Login</button></a>
+    <a href="dist/dlogin.php"> <button style="font-size:25px ;background-color:	#008080">Distributor Login</button></a>
+</div>
+</div>
+</body>
+</html>
